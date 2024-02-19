98e26169046a1e53d49b7402af6d0d8de90bd488
snikersovich <146052110+snikersovich@users.noreply.github.com>
2024-02-19 12:56:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#Задача 1
+def horse2(sell):
+    alphavit = 'abcdefgh'
+    steps = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
+    x = alfavit.find(cell[0]) +1
+    y = int(cell[1])
+    for xy in steps:
+        x1, y1 = x + xy[0], y + xy[1]
+        if 0 < x1 < 9 and 0 < y1 < 9:
+            print(f'{alfavit[x1 - 1]}{y1}')
+
+#Задача 2
 def template(a, b, c):
     if a + b > c > b and b + c > a:
         print(f'Периметр: {a + b + c}')
@@ -13,3 +25,5 @@ def template(a, b, c):
             print(f'Равносторонний: нет')
     else:
         print('None')
+
+#Задача 3
