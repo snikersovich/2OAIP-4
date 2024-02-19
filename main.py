1cadcadc7de61a07c4a7f2d886dfe15aeadf11fd
snikersovich <146052110+snikersovich@users.noreply.github.com>
2024-02-19 13:00:59 +1000

    main.py

--- main.py
+++ main.py
@@ -27,3 +27,8 @@ def template(a, b, c):
         print('None')
 
 #Задача 3
+def func_table(exp, x, y):
+    res = [[eval(exp.replace('x', str(i)).replace('y', str(j))) for j in range(y + 1)] for i in range(x + 1)]
+    [print(*i, sep='\t') for i in res]
+ 
+func_table('x ** 2 + y', 3, 5)
